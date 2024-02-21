ec8da46921162aca5c59387f3cb7618509033b30
Arthur Borges <arthurborges.senai564@gmail.com>
2024-02-21 14:38:01 -0300

    Feat: Add task functionality and task list display

--- App.jsx
+++ App.jsx
@@ -1,21 +1,91 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [task, setTask] = useState("");
+	const [tasks, setTasks] = useState([]);
+	
+	const addTask = () => {
+	  if (task.trim().length > 0) {
+	    setTasks([...tasks, { id: Math.random().toString(), value: task }]);
+	    setTask("");
+	  }
+	};
+  const removeTask = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <Text>!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.form}>
+      <TextInput
+        placeholder="Digite uma nova tarefa"
+        style={styles.inputLine}
+        onChangeText={setTask}
+        value={task}
+      />
+      <TouchableOpacity
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Adicionar</Text>
+      </TouchableOpacity>
     </View>
+    <View style={styles.taskContainer}>
+  {tasks.length === 0 ? (
+    <Text style={styles.taskTextRed}>Nenhuma Tarefa Cadastrada</Text>
+  ) : (
+    <Text style={styles.taskTextGreen}>Tarefas Cadastradas</Text>
+  )}
+</View>
+    <StatusBar style="auto" />
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
   },
+  form: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  inputLine: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+    padding: 10,
+    width: '75%',
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+  },
+  taskContainer: {
+    marginBottom: 20,
+  },
+  taskTextGreen: {
+    textAlign: 'center',
+    color: 'green',
+    fontSize: 20,
+  },
+  taskTextRed: {
+    color: 'red',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  
 });
+
